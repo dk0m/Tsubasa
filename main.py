cf968f8eff98d62ebfb6e7a1e4985526e839c184
dk0m <hookirp@gmail.com>
2025-02-11 15:06:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -70,4 +70,3 @@ for volIndex in volsData:
 
                 with open(f'{dirPath}/{imgIndex}.jpg', 'wb') as chapterImg:
                     chapterImg.write(r.content)
-                    chapterImg.close()
